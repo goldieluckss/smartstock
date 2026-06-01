d2dd1d947721ebdbe145d403ebadee5933f79e7e
GoldieLuckss <liyensey@gmail.com>
2026-06-01 14:09:31 +0800

    l

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,12 +3,15 @@ const env = require("./env");
 
 const pool = mysql.createPool({
   host: env.dbHost,
-  port: env.dbPort,
+  port: Number(env.dbPort),
   user: env.dbUser,
   password: env.dbPassword,
   database: env.dbName,
   waitForConnections: true,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 async function checkDatabaseConnection() {
